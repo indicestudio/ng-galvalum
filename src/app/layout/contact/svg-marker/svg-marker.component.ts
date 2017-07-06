4190cc2ba68156762c0a4397df2d4a3e23126e52
indicestudio <indicestudio@gmail.com>
2017-07-05 22:28:17 -0400

    elimine ngmodule de svg-marker

--- src/app/layout/contact/svg-marker/svg-marker.component.ts
+++ src/app/layout/contact/svg-marker/svg-marker.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-
-@NgModule({
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-})
 @Component({
   selector: 'app-svg-marker',
   templateUrl: './svg-marker.component.html',
