e93fe83d9c18b29530d8446d3b9bc9ef3da2fd60
indicestudio <indicestudio@gmail.com>
2017-07-14 01:21:39 -0400

    modificacion overflow del container, saque animaciones, cambie timer por observables

--- src/app/layout/slideshow/slideshow.component.ts
+++ src/app/layout/slideshow/slideshow.component.ts
@@ -1,38 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Slide } from '../../shared/slide.model';
-
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
-  styleUrls: ['./slideshow.component.css'],
-  animations: [
-    trigger('slideState', [
-      state('inactive', style({
-        opacity: '0',
-        transform: 'translateX(100%)'
-      })),
-      state('active', style({
-        opacity: '1',
-        transform: 'translateX(0)'
-      })),
-      transition('inactive => active', animate('2000ms ease-in-out'))
-    ]),
-    trigger('captionState', [
-      state('inactive', style({
-        opacity: '0',
-        transform: 'translateY(-100%)'
-      })),
-      state('active', style({
-        opacity: '1',
-        transform: 'translate(-50%, -50%)'
-      })),
-      transition('inactive <=> active', animate('2000ms ease-in-out'))     
-    ])
-  ]
+  styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, OnDestroy {
 
   // Slide Data
   slides: Slide[] = [
@@ -53,61 +27,13 @@ export class SlideshowComponent implements OnInit {
     }
   ];
 
-  slideIndex: number = 0;
-  slideState: string = 'inactive';
-  
-  timeBetweenSlides: number = 4000;
-  slideTranslateX: number = 0;
-
-  constructor() {}
+  constructor() {  }
 
   ngOnInit() {
-    this.startSlides();
   }
 
-  startSlides() {
-    if (this.slideTranslateX === (this.slides.length - 1) * -100) {
-      this.slideTranslateX = 0;
-    }
-    setTimeout(()=>{
-      this.slideState = 'active';
-      setTimeout(()=>{
-        this.slideTranslateX -= 100;
-        setTimeout(()=>{
-          this.startSlides();
-        },this.timeBetweenSlides);
-      }, this.timeBetweenSlides);
-    }, 450);
+  ngOnDestroy() {
   }
-
-
-
-/*
-  ngOnInit() {
-    let timer = setTimeout(() => {
-      this.changeSlideState();
-    }, 500)
-  }
-
-  changeSlideState(){
-    this.slideState = 'active';
-    let timer = setTimeout(() => {
-      this.slideState = 'inactive';
-      this.changeSlide();
-    }, this.slideTime);
-  }
-
-  changeSlide(){
-    let timer = setTimeout(() => {
-      this.slideIndex++;
-      if (this.slideIndex > 2) {
-        this.slideIndex = 0;
-      }
-      this.changeSlideState();
-    }, this.slideTime);
-  }
-  */
-
 
 
 
