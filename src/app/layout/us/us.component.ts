ff972298bd368ab5686711842a8013055828c007
indicestudio <indicestudio@gmail.com>
2017-07-21 15:36:30 -0400

    colores teal al azul y cambios altura nosotros.

--- src/app/layout/us/us.component.ts
+++ src/app/layout/us/us.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class UsComponent implements OnInit {
 
   borderWindow = 1920;
-  svgCoordinates: string = '0,0 0,278 ' + this.borderWindow +',278 ' + this.borderWindow + ',272 587,272 587,197 562,197 562,186 604,186 548.896,131.051 493.945,186 536,186 536,197 511,197 511,272 346,272';
-  svgColor = "#fff";
+  svgCoordinates: string = '0,0 0,278 '
+    + this.borderWindow
+    + ',278 '
+    + this.borderWindow
+    + ',272 587,272 587,197 562,197 562,186 604,186 548.896,131.051 493.945,186 536,186 536,197 511,197 511,272 346,272';
+  svgColor = '#fff';
   constructor() { }
 
   ngOnInit() {
